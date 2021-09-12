291311e6c263c0c4f40b1a003349958af75a2357
maropook <ii440092@icloud.com>
2021-09-12 09:47:22 +0900

    filterを追加し，results:[]の形のresuponseが帰ってくるように変更帰ってくるように変更

--- book_management/settings.py
+++ book_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
 
     'corsheaders',
     'book',
@@ -133,3 +134,14 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',  # ここに追加
     'http://localhost:8000',
 ]
+
+APPEND_SLASH = False
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 200,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )}
